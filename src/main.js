76568520803dc60ac39c254dde000da4ac1792d8
Peter Lu <peterlu@cw.com.tw>
2026-04-15 00:53:03 +0800

    chore: clarify deferred router registration in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+// router imported and registered in Task 6 (src/router/index.js)
 
 const app = createApp(App)
 
